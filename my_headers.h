cf2312dfa71cda8096763eeb8649b99c95466084
AH-HESHAM <96313662+AH-HESHAM@users.noreply.github.com>
2021-12-24 16:41:35 +0200

    Update my_headers.h

--- my_headers.h
+++ my_headers.h
@@ -1,8 +1,9 @@
 #ifndef MY_HEADERS_H_INCLUDED
 #define MY_HEADERS_H_INCLUDED
 void startNewGame();
+int checkScore(int score, int row, int col ,int size, int array[size][size],int *turn,int arrayPlayer[size][size]);
 void AI(int size, int array[size][size], int *rowx , int *colx);
 //void undo(int noMoves, int numOfPlayers, int size,  int array[size][size], int array1[size][size], int array2[size][size], char active[size][size], int row, int col );
-
+void loadedGames();
 
 #endif // MY_HEADERS_H_INCLUDED
