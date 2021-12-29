c26e8bb32a9dd3b7e3e3a3f48e6656304758f664
AbdelrahmanWael2 <96313471+AbdelrahmanWael2@users.noreply.github.com>
2021-12-29 18:39:51 +0200

    Update start new game.c

--- start new game.c
+++ start new game.c
@@ -5,30 +5,7 @@
 #define BLK "\e[0;30m"
 #define RED "\e[0;31m"
 #define GRN "\e[0;32m"
-#define YEL "\e[0;33m"
-#define BLU "\e[0;34m"
-#define MAG "\e[0;35m"
-#define CYN "\e[0;36m"
-#define WHT "\e[0;37m"
-#define BLKB "\e[40m"
-#define REDB "\e[41m"
-#define GRNB "\e[42m"
-#define YELB "\e[43m"
-#define BLUB "\e[44m"
-#define MAGB "\e[45m"
-#define CYNB "\e[46m"
-#define WHTB "\e[47m"
-#define BLKB "\e[40m"
-#define REDB "\e[41m"
-#define GRNB "\e[42m"
-#define YELB "\e[43m"
-#define BLUB "\e[44m"
-#define MAGB "\e[45m"
-#define CYNB "\e[46m"
-#define WHTB "\e[47m"
-#include <ctype.h>
 #include <string.h>
-#include <windows.h>
 #include "my_headers.h"
 
 
