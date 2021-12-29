55cfa2753ac5f301959a86119c9d2d9b4b88ea17
AH-HESHAM <96313662+AH-HESHAM@users.noreply.github.com>
2021-12-29 14:10:32 +0200

    Update my_headers.h

--- my_headers.h
+++ my_headers.h
@@ -1,9 +1,11 @@
 #ifndef MY_HEADERS_H_INCLUDED
 #define MY_HEADERS_H_INCLUDED
 void startNewGame();
-int checkScore(int score, int row, int col ,int size, int array[size][size],int *turn,int arrayPlayer[size][size]);
 void AI(int size, int array[size][size], int *rowx , int *colx);
-//void undo(int noMoves, int numOfPlayers, int size,  int array[size][size], int array1[size][size], int array2[size][size], char active[size][size], int row, int col );
+int checkScore(int score, int row, int col ,int size, int array[size][size],int *turn,int arrayPlayer[size][size]);
+void saveGame(int size,int turn,int noPlayers,int noMovesLeft,int score1,int score2,char player1[10],char player2[10] ,int array[size][size], int array1[size][size], int array2[size][size]);
+void checkRank(int score, char playerName[10]);
+void setToZero(int size, int arr[size][size]);
 void loadedGames();
 
 #endif // MY_HEADERS_H_INCLUDED
