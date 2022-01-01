06d9e0fad23781af640e5d64dacefbff168179f4
AH-HESHAM <96313662+AH-HESHAM@users.noreply.github.com>
2022-01-01 18:02:28 +0200

    Update my_headers.h

--- my_headers.h
+++ my_headers.h
@@ -7,8 +7,9 @@ void saveGame(int size,int turn,int noPlayers,int noMovesLeft,int score1,int sco
 void checkRank(int score, char playerName[10]);
 void setToZero(int size, int arr[size][size]);
 void loadedGames();
-void redo1(int *score1, int *score2 ,int i,int stor[20],int stoc[20],int size,  int array2[size][size], int array1[size][size], int array[size][size], char active[size][size], int turn, int *ii);
-void  undo1(int *score1, int *score2, int i,int stor[20],int stoc[20],int size,  int array2[size][size], int array1[size][size], int array[size][size], char active[size][size],int *ii);
+void redo1(int *score1, int *score2 ,int counter,int stor[20],int stoc[20],int size,  int array2[size][size], int array1[size][size], int array[size][size], char active[size][size], int turn,  int removedline[20], int undos, char passive[size][size]);
+void  undo1(int *score1, int *score2 ,int counter,int stor[20],int stoc[20],int size,  int array2[size][size], int array1[size][size], int array[size][size], char active[size][size],   int removedline[20], int undos);
 void redo2(int *score1, int *score2, int counter, int size, int array[size][size], int array1[size][size], int array2[size][size], char active[size][size], int stor[20], int stoc[20], int turn, char passive[size][size], int removedline[20], int undos);
 void undo2(int size, int *score1, int *score2, int array1[size][size], int array2[size][size], int stor[30], int stoc[30], int counter, int array[size][size], char active[size][size], int removedline[20], int undos);
 #endif // MY_HEADERS_H_INCLUDED
+
